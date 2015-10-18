414b4c202e55e0b8a43edee0902a7ce9e9a1f20b
Rahman Adianto <rahman.adianto@gmail.com>
2015-10-18 20:12:17 +0700

    Fix conflict

--- src/common.h
+++ src/common.h
@@ -3,7 +3,6 @@
  * Description : This file contain all configuration and data structure for sliding window protocol project 
  */
 
-<<<<<<< HEAD
 #ifndef COMMON_H
 #define COMMON_H
 
@@ -16,12 +15,6 @@
 #include <arpa/inet.h> // inet_pton
 #include <pthread.h>
 
-=======
-
-#ifndef COMMON_H
-#define COMMON_H
-
->>>>>>> 8ad339bf37ea2fe8ea0030343713518d78fc07f0
 /* ASCII CONST */
 #define SOH 1				// Start of Header Character 
 #define STX 2				// Start of Text Character
@@ -30,12 +23,9 @@
 #define NAK 21			// Negatif Acknowledgement
 #define Endfile 26	// End of file Character
 
-<<<<<<< HEAD
 /* Constant */
 #define MAXLEN 1024
 
-=======
->>>>>>> 8ad339bf37ea2fe8ea0030343713518d78fc07f0
 typedef unsigned char Byte;
 
 struct Frame {
@@ -43,16 +33,11 @@ struct Frame {
 	unsigned int frame_number;
 	unsigned int stx;
 	unsigned int etx;
-<<<<<<< HEAD
 	unsigned int checksum;
-=======
-	Byte checksum;
->>>>>>> 8ad339bf37ea2fe8ea0030343713518d78fc07f0
 	Byte data;
 };
 
 struct Ack {
-<<<<<<< HEAD
 	unsigned int value;
 	unsigned int frame_number;
 	unsigned int checksum;
@@ -66,11 +51,3 @@ unsigned int hash(unsigned int input) {
 }
 
 #endif
-=======
-	unsigned int ack;
-	unsigned int frame_number;
-	Byte checksum;
-};
-
-#endif
->>>>>>> 8ad339bf37ea2fe8ea0030343713518d78fc07f0
